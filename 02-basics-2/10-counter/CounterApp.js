8f9c67a50739490c15985d4a9ef8f3148ae40a0c
Grigory Sayfullin <108182430+Dantex174@users.noreply.github.com>
2025-03-13 00:27:11 +0300

    Выполнил counter

--- 02-basics-2/10-counter/CounterApp.js
+++ 02-basics-2/10-counter/CounterApp.js
@@ -1,26 +1,51 @@
-import { defineComponent } from 'vue'
+import {defineComponent, ref} from 'vue'
 
 export default defineComponent({
   name: 'CounterApp',
 
-  setup() {},
+  setup() {
+    const counter = ref(0);
+
+    const modifyCounter = (event) => {
+      switch (event.target.getAttribute('aria-label')) {
+
+        case 'Decrement':
+          counter.value--;
+          break;
+
+        case 'Increment':
+          counter.value++;
+          break;
+      }
+    }
+
+    return {
+      counter,
+      modifyCounter
+    }
+  },
 
   template: `
     <div class="counter">
+
       <button
         class="button button--secondary"
         type="button"
         aria-label="Decrement"
-        disabled
+        :disabled="counter <= 0"
+        @click="modifyCounter($event)"
       >➖</button>
 
-      <span class="count" data-testid="count">0</span>
+      <span class="count" data-testid="count">{{ counter }}</span>
 
       <button
         class="button button--secondary"
         type="button"
         aria-label="Increment"
+        :disabled="counter >= 5"
+        @click="modifyCounter($event)"
       >➕</button>
+
     </div>
   `,
 })
